6b9621f457cdd0d0447f65652912730c5b2cb046
Jay Han <hjay@fortinet.com>
2022-01-25 17:43:43 -0800

    CH4

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {HEROES} from '../mock-heroes';
 import {Hero} from '../hero.interface';
+import {HeroService} from '../hero.service';
+import {MessageService} from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,17 +9,22 @@ import {Hero} from '../hero.interface';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor() {
-  }
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes() {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
 }
